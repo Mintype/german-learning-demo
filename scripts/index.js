4341300576601096a33e30960f9d3a616e0fda04
SmallPlayz <900raiyan@gmail.com>
2024-01-15 10:56:05 -0500

    Added some more styles/js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 function optionOneClicked() {
     console.log("Option One Clicked.");
+
+    let myDiv = document.getElementById('option1');
+    
+    myDiv.classList.add('optionCorrect');
+
 }
 
 function optionTwoClicked() {
