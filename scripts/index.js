10f6168371f33fc18510032a5593600d5cae6938
SmallPlayz <900raiyan@gmail.com>
2024-01-14 22:05:44 -0500

    Added onclick functions.

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+function optionOneClicked() {
+    console.log("Option One Clicked.");
+}
+
+function optionTwoClicked() {
+    console.log("Option Two Clicked.");
+}
+
+function optionThreeClicked() {
+    console.log("Option Three Clicked.");
+}
+
+function optionFourClicked() {
+    console.log("Option Four Clicked.");
+}
